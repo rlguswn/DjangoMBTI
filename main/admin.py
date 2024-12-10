3e05726f14f6fc25e24b261aa5059efa594c9214
주기현 <0318joo@naver.com>
2024-12-10 18:29:42 +0900

    #2 admin에 기본 DB 모델 추가

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from main.models import Question, Option, Mbti
+
+
 # Register your models here.
+@admin.register(Mbti)
+class MbtiAdmin(admin.ModelAdmin):
+    list_display = ('mbti', 'text')
+    search_fields = ('mbti', 'text')
+    ordering = ('mbti',)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('dimension', 'text')
+    search_fields = ('dimension', 'text')
+    ordering = ('dimension',)
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'text', 'score')
+    search_fields = ('question', 'text', 'score')
+    ordering = ('question', 'score')
