2c61e6ee20d0d4d64eff008ceb97dcc44ff4ebe6
주기현 <0318joo@naver.com>
2024-12-12 23:04:32 +0900

    #2 질문과 질문에 대한 선택지를 반환하는 view 생성

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from main.Serializers import QuestionSerializer, MbtiSerializer
+from main.models import Question, Option
+
 
 # Create your views here.
+class QuestionView(APIView):
+    def get(self, request):
+        queryset = Question.objects.prefetch_related('option_set').all()
+        serializer = QuestionSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
