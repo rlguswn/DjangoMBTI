173efbdfb2607963b1cfd510b14eef926b9168cf
주기현 <0318joo@naver.com>
2024-12-13 16:38:14 +0900

    #2 mbti view 생성

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from main.Serializers import QuestionSerializer, MbtiSerializer
-from main.models import Question, Option
+from main.models import Question, Option, Mbti
 
 
 # Create your views here.
@@ -13,3 +13,24 @@ class QuestionView(APIView):
         serializer = QuestionSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class MbtiView(APIView):
+    def post(self, request):
+        answers = request.data.get('answer')
+        scores = {'EI': 0, 'SN': 0, 'TF': 0, 'JP': 0}
+
+        for question_id, option_id in answers.items():
+            option = Option.objects.get(id=option_id)
+            dimension = option.question.dimension
+            scores[dimension] += option.score
+
+        mbti = ''
+        for dimension, score in scores.items():
+            if score > 0:
+                mbti += dimension[0]
+            elif score < 0:
+                mbti += dimension[1]
+
+        queryset = Mbti.objects.get(mbti=mbti)
+        serializer = MbtiSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
