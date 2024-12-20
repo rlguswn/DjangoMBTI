a1c2e8426871f86abe0190850bd8efb21457d9bb
주기현 <0318joo@naver.com>
2024-12-20 19:59:47 +0900

    #2 각 View의 Swagger 설명 작성

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.db import DatabaseError
 from django.core.exceptions import ObjectDoesNotExist
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
@@ -11,6 +13,56 @@ from main.models import Question, Option, Mbti
 
 # Create your views here.
 class QuestionView(APIView):
+    @swagger_auto_schema(
+        operation_id='QustionView',
+        operation_description='질문과 질문에 대한 선택지 불러오기',
+        responses={
+            status.HTTP_200_OK: openapi.Response(
+                'Success',
+                schema=openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(
+                        type=openapi.TYPE_OBJECT,
+                        properties={
+                            'id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Qustion id'),
+                            'text': openapi.Schema(type=openapi.TYPE_STRING, description='Question text'),
+                            'options': openapi.Schema(
+                                type=openapi.TYPE_ARRAY,
+                                items=openapi.Schema(
+                                    type=openapi.TYPE_OBJECT,
+                                    properties={
+                                        'id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Option id'),
+                                        'text': openapi.Schema(type=openapi.TYPE_STRING, description='Option text'),
+                                    }
+                                ),
+                                description='Options for Qustion'
+                            )
+                        }
+                    )
+                )
+            ),
+            status.HTTP_400_BAD_REQUEST: openapi.Response(
+                'Bad Request', schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'error': openapi.Schema(
+                            type=openapi.TYPE_STRING, description='400 error message'
+                        )
+                    }
+                )
+            ),
+            status.HTTP_500_INTERNAL_SERVER_ERROR: openapi.Response(
+                'Internal Server Error', schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'error': openapi.Schema(
+                            type=openapi.TYPE_STRING, description='500 error message'
+                        )
+                    }
+                )
+            )
+        }
+    )
     def get(self, request):
         try:
             queryset = Question.objects.prefetch_related('option_set').all()
@@ -30,6 +82,47 @@ class QuestionView(APIView):
 
 
 class MbtiView(APIView):
+    @swagger_auto_schema(
+        operation_id='MbtiView',
+        operation_description='질문과 질문에 대한 선택지 불러오기',
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+
+            }
+        ),
+        responses={
+            status.HTTP_200_OK: openapi.Response(
+                'Success', schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'mbti': openapi.Schema(type=openapi.TYPE_STRING, description='MBTI'),
+                        'text': openapi.Schema(type=openapi.TYPE_STRING, description='MBTI description')
+                    }
+                )
+            ),
+            status.HTTP_400_BAD_REQUEST: openapi.Response(
+                'Bad Request', schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'error': openapi.Schema(
+                            type=openapi.TYPE_STRING, description='400 error message'
+                        )
+                    }
+                )
+            ),
+            status.HTTP_500_INTERNAL_SERVER_ERROR: openapi.Response(
+                'Internal Server Error', schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'error': openapi.Schema(
+                            type=openapi.TYPE_STRING, description='500 error message'
+                        )
+                    }
+                )
+            )
+        }
+    )
     def post(self, request):
         try:
             answers = request.data.get('answer')
